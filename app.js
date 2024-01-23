e35e4729a461141d38ab1f216960b0bd524a4392
Guilherme Lima <guilhermevictorrocha@gmail.com>
2024-01-23 11:00:46 -0300

    fix: corrigindo logs da request

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ server.use(function (req, res, next) {
 });
 
 
-async function makeRequest(body, tokenId, tokenSecret, url, httpMethod) {
+async function makeRequest(body, requestName, tokenId, tokenSecret, url, httpMethod) {
 	const requestResponse = await fetch(url, {
 		method: httpMethod,
 		headers: {
@@ -25,14 +25,13 @@ async function makeRequest(body, tokenId, tokenSecret, url, httpMethod) {
 	});
 
 	if (requestResponse.ok) {
-		console.log(`Requisição enviada com sucesso: ${body ? body.name : httpMethod}`);
+		console.log(`Requisição enviada com sucesso: ${requestName ? requestName : httpMethod}`);
 		return await requestResponse.json();
 	} else {
 		console.log(JSON.stringify(body, null, 2));
 		console.error(`Algo de errado ocorreu ao realizar a requisição: Status ${requestResponse.status}, JSON: ${JSON.stringify(await requestResponse.json(), null, 2)}`);
 		return { error: `Algo de errado ocorreu ao realizar a requisição: Status ${requestResponse.status}, JSON: ${JSON.stringify(await requestResponse.json())}` };
 	}
-
 }
 
 async function sendRequestsByJsons(jsonArray, tokenId, tokenSecret, url, httpMethod) {
@@ -40,7 +39,7 @@ async function sendRequestsByJsons(jsonArray, tokenId, tokenSecret, url, httpMet
 
 	for (const json of jsonArray) {
 		try {
-			responses.push(await makeRequest(JSON.stringify(json), tokenId, tokenSecret, url, httpMethod));
+			responses.push(await makeRequest(JSON.stringify(json), json.name, tokenId, tokenSecret, url, httpMethod));
 		} catch (error) {
 			console.error(`Ocorreu um erro: ${error}`)
 			responses.push({ error: `Erro ao realizar requisição (${json.name}): ${error}`, json });
@@ -66,7 +65,7 @@ server.post('/', (req, res) => {
 			if (json.httpMethod === 'POST' || json.httpMethod === 'PUT') {
 				responses = await sendRequestsByJsons(json.requestJsons, json.tokenId, json.tokenSecret, json.url, json.httpMethod);
 			} else {
-				responses = await makeRequest(null, json.tokenId, json.tokenSecret, json.url, json.httpMethod);
+				responses = await makeRequest(null, '', json.tokenId, json.tokenSecret, json.url, json.httpMethod);
 			}
 
 			console.log(`Quantidade de respostas: ${responses.length}`);
